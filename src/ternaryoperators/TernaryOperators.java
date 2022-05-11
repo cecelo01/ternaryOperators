dd63958cdb4a134b33bcb5ed345e333b176664fb
Marcelo Vieira <48066643+cecelo01@users.noreply.github.com>
2022-05-11 10:44:18 -0300

    Initial commit

---
+++ src/ternaryoperators/TernaryOperators.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ternaryoperators;
+
+/**
+ *
+ * @author Marce
+ */
+public class TernaryOperators {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+    
+        int n1=5, n2=10, r;
+        r = n1 > n2 ? n1+n2 : n1-n2;
+        System.out.println(r);  
+        
+        
+        
+
+    }
+    
+}
